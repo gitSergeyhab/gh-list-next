6899127a01349a01d06f26325bd71fabe366a163
Sergey <redmoloch@yandex.ru>
2024-10-21 17:43:52 +0300

    fix(gh-list-next-4): Fix SSG (slug=>id)

--- app/repos/[id]/page.tsx
+++ app/repos/[id]/page.tsx
@@ -5,27 +5,29 @@ import { JsonText } from "@/shared/json-text";
 export interface RepoParams {
   params: {
     id: number;
-  }
+  };
 }
 
-export async function generateStaticParams () {
-    const repos = await fetchRepos();
-    return repos.map((repo) => ({ slug: repo.id.toString() }));
+export async function generateStaticParams() {
+  const repos = await fetchRepos();
+  return repos.map((repo) => ({ id: repo.id.toString() }));
 }
 
-export async function generateMetadata({params: {id}}: RepoParams) {
-  const repo = await fetchRepoById(id)
+export async function generateMetadata({ params: { id } }: RepoParams) {
+  const repo = await fetchRepoById(id);
   return {
     title: `repo: ${repo.full_name}`,
   };
 }
 
-export default async function Repo({params: {id}}: RepoParams) {
-    const repo = await fetchRepoById(id)
-    return (
-        <section>
-          <h1 className="app__title">Repository (Example of SSG): {repo.full_name}</h1>
-          <JsonText>{JSON.stringify(repo, null, 2)}</JsonText>
-        </section>
-    );
+export default async function Repo({ params: { id } }: RepoParams) {
+  const repo = await fetchRepoById(id);
+  return (
+    <section>
+      <h1 className="app__title">
+        Repository (Example of SSG): {repo.full_name}
+      </h1>
+      <JsonText>{JSON.stringify(repo, null, 2)}</JsonText>
+    </section>
+  );
 }
